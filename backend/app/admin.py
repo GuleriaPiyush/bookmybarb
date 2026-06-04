48fe8189a63f55402145ef49c56679a72d38f8f9
GuleriaPiyush <hguleriaedu84@gmail.com>
2026-06-04 09:32:55 +0530

    added models

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Service, Slot, Booking,ShopSetting
 
 # Register your models here.
+
+admin.site.register(Slot)
+admin.site.register(Service)
+admin.site.register(Booking)
+admin.site.register(ShopSetting)
